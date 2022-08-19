fbe6ded046fdbe9cc9a900bbf8e85bc514ba36ec
wttch（王冲） <wttch@wttch.com>
2022-08-19 11:27:31 +0800

    Initial Commit

---
+++ CMU 15445/CMU_15445.h
@@ -0,0 +1,18 @@
+//
+//  CMU_15445.h
+//  CMU 15445
+//
+//  Created by Wttch on 2022/8/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CMU_15445.
+FOUNDATION_EXPORT double CMU_15445VersionNumber;
+
+//! Project version string for CMU_15445.
+FOUNDATION_EXPORT const unsigned char CMU_15445VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMU_15445/PublicHeader.h>
+
+
